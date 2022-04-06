1bb7acd3c6a6de158f1aaf4a42516490e464b74a
JakubKKK <kubamen9@wp.pl>
2022-04-06 20:20:48 +0200

    html templates

--- store/Products/views.py
+++ store/Products/views.py
@@ -11,12 +11,18 @@ def index(request):
 
 def kategoria(request, id):
     kategoria_user = Kategoria.objects.get(pk=id)
-    return HttpResponse(kategoria_user.nazwa)
+    kategoria_produkt = Produkty.objects.filter(kategoria = kategoria_user)
+    kategorie = Kategoria.objects.all()
+    dane = {'kategoria_user': kategoria_user,
+            'kategoria_produkt': kategoria_produkt,
+            'kategorie': kategorie }
+    return render(request, 'kategoria_produkt.html', dane)
 
 
 def produkt(request, id):
     produkt_user = Produkty.objects.get(pk=id)
-    dane = {'produkt_user': produkt_user}
+    kategorie = Kategoria.objects.all()
+    dane = {'produkt_user': produkt_user, 'kategorie': kategorie}
     return render(request, 'produkt.html', dane)
 
 
